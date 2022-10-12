47074d290a49f24fc12cd35d33a7211ef6e787ca
uplostudio <110473855+uplostudio@users.noreply.github.com>
2022-10-12 20:24:45 +0200

    Create login.js

---
+++ login.js
@@ -0,0 +1,26 @@
+$(".is-login-popup").on("click", function () {
+  $("[app='login_modal'").addClass("modal--open");
+});
+
+$("[app='login']").on("submit", function (event) {
+  event.preventDefault();
+  event.stopPropagation();
+  const errorInfo = $(this).find(".w-form-fail");
+  $.ajax({
+    url: "https://www.shoper.pl/ajax.php",
+    headers: {},
+    method: "POST",
+    data: {
+      action: $("[app='login']").attr("action"),
+      host: document.querySelector("[app='host']").value
+    },
+    success: function (data) {
+      if (data.status === 1) {
+        window.location.href = data.redirect;
+      } else {
+        console.log(data);
+        errorInfo.css("display", "block");
+      }
+    }
+  });
+});
