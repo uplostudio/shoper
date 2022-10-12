f849a6ae5ef2919678e4acbd9bbcf515f272f2b5
uplostudio <110473855+uplostudio@users.noreply.github.com>
2022-10-12 20:23:26 +0200

    Create trial-step-2.js

---
+++ trial-step-2.js
@@ -0,0 +1,24 @@
+$("[app='create_trial_step2']").on("submit", function (event) {
+  event.preventDefault();
+  event.stopPropagation();
+  const errorInfo = $(this).find(".w-form-fail");
+  $.ajax({
+    url: "https://www.shoper.pl/ajax.php",
+    headers: {},
+    method: "POST",
+    data: {
+      action: "create_trial_step1",
+      email: localStorage.getItem("trial_email"),
+      phone: $(this).find("[app='phone']").val()
+    },
+    success: function (data) {
+      console.log(data);
+      if (data.status === 1) {
+        window.location.href = "https://www.shoper.pl/zaloz-sklep/";
+      } else {
+        errorInfo.css("display", "block");
+        errorInfo.html("Podany numer jest nieprawidÅ‚owy");
+      }
+    }
+  });
+});
