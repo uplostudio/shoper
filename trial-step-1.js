c184d9b3b2c1d8cddf7e80d76d1d0840b0fc5f22
uplostudio <110473855+uplostudio@users.noreply.github.com>
2022-10-19 21:08:41 +0200

    Update trial-step-1.js

--- trial-step-1.js
+++ trial-step-1.js
@@ -1,3 +1,48 @@
+let inputsStepOne = document.querySelectorAll("[app='create_trial_step1'] input:not([type='radio']):not([type='checkbox']):not([type='password']):not([type='submit'])")
+
+
+inputsStepOne.forEach((n) => {
+  // Control Blur Step One
+  n.addEventListener("blur", () => {
+    let data;
+    let element = document.querySelector("[data-name='create_trial_step1']");
+    let elementId = element.id;
+
+    if (window.dataLayer) {
+      data = {
+        event: 'controlBlur',
+        formId: elementId,
+        controlName: n.getAttribute("data-name"),
+        controlType: n.type,
+        controlValue: n.value
+      };
+
+      dataLayer.push(data);
+      console.log(dataLayer);
+    }
+  })
+  // Control Focus Step One
+  n.addEventListener("focus", () => {
+    let data;
+    let element = document.querySelector("[data-name='create_trial_step1']");
+    let elementId = element.id;
+
+    if (window.dataLayer) {
+      data = {
+        event: 'controlFocus',
+        formId: elementId,
+        controlName: n.getAttribute("data-name"),
+        controlType: n.type,
+        controlValue: n.value
+      };
+
+      dataLayer.push(data);
+      console.log(dataLayer);
+    }
+  })
+})
+
+
 let createTrialStepOne = document.querySelectorAll(
   "[app='create_trial_step1']"
 );
