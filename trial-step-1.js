e51e6cde12609a04223815bed8bb9991952f8353
uplostudio <110473855+uplostudio@users.noreply.github.com>
2022-10-19 20:49:57 +0200

    Update trial-step-1.js

--- trial-step-1.js
+++ trial-step-1.js
@@ -1,34 +1,104 @@
-$(".is-trial-popup, [app='open_trial_modal_button']").on("click", function () {
-  $("[app='create_trial_step1_modal']").addClass("modal--open");
-});
+let createTrialStepOne = document.querySelectorAll(
+  "[app='create_trial_step1']"
+);
 
-$("[app='create_trial_step1']").on("submit", function (event) {
-  event.preventDefault();
-  event.stopPropagation();
-  const errorInfo = $(this).find(".w-form-fail");
-  $.ajax({
-    url: "https://www.shoper.pl/ajax.php",
-    headers: {},
-    method: "POST",
-    data: {
-      action: "create_trial_step1",
-      email: $(this).find("[app='email']").val()
-    },
-    success: function (data) {
-      console.log(data);
-      if (data.status === 1) {
-        $("[app='create_trial_step1_modal']").removeClass("modal--open");
-        $("[modal='create_trial_step2']").addClass("modal--open");
-        $("[app='trial-domain']").html(data.host);
+console.log(createTrialStepOne);
+
+createTrialStepOne.forEach((n) => {
+  n.addEventListener("submit", (e) => {
+    e.preventDefault();
+    e.stopPropagation();
+
+    let url = "https://www.shoper.pl/ajax.php";
+
+    fetch(url, {
+      headers: {
+        "Content-Type": "application/json",
+      },
+      method: "POST",
+      body: JSON.stringify({
+        action: "create_trial_step1",
+        email: n.querySelector("[app='email']").value,
+      }),
+    }).then((response) => {
+      let status = response.status;
+      console.log(response);
+
+      if (status === 200) {
+        document
+          .querySelector("[app='create_trial_step1_modal']")
+          .classList.remove("modal--open");
+        document
+          .querySelector("[modal='create_trial_step2']")
+          .classList.add("modal--open");
+        // $("[app='trial-domain']").html(data.host);
+        // MyTrackEvent Success (Step One)
+        if (window.dataLayer) {
+          data = {
+            eventCategory: "Button form sent",
+            eventAction: n.querySelector("input[type='submit']:nth-child(1)")
+              .value,
+            eventLabel: window.location.pathname,
+            eventType: n.querySelector("[app='email']").value,
+          };
+
+          dataLayer.push(data);
+          console.log(dataLayer);
+        }
       } else {
         errorInfo.css("display", "block");
         errorInfo.html("Podany email jest nieprawidłowy");
-        if (data.code === 2) {
-          errorInfo.html(
-              "Uruchomiłeś co najmniej cztery wersje testowe sklepu w zbyt krótkim czasie. Odczekaj 24h od ostatniej udanej próby, zanim zrobisz to ponownie."
-            );
+        // MyTrackEvent Error (Step One)
+        if (window.dataLayer) {
+          data = {
+            eventCategory: "Button form error",
+            eventAction: n.querySelector("input[type='submit']:nth-child(1)")
+              .value,
+            eventLabel: window.location.pathname,
+            eventType: n.querySelector("[app='email']").value,
+          };
+
+          dataLayer.push(data);
+          console.log(dataLayer);
         }
       }
-    }
+    });
   });
 });
+
+
+
+// $(".is-trial-popup, [app='open_trial_modal_button']").on("click", function () {
+//   $("[app='create_trial_step1_modal']").addClass("modal--open");
+// });
+
+// $("[app='create_trial_step1']").on("submit", function (event) {
+//   event.preventDefault();
+//   event.stopPropagation();
+//   const errorInfo = $(this).find(".w-form-fail");
+//   $.ajax({
+//     url: "https://www.shoper.pl/ajax.php",
+//     headers: {},
+//     method: "POST",
+//     data: {
+//       action: "create_trial_step1",
+//       email: $(this).find("[app='email']").val()
+//     },
+//     success: function (data) {
+//       console.log(data);
+//       if (data.status === 1) {
+//         $("[app='create_trial_step1_modal']").removeClass("modal--open");
+//         $("[modal='create_trial_step2']").addClass("modal--open");
+//         $("[app='trial-domain']").html(data.host);
+//       } else {
+//         errorInfo.css("display", "block");
+//         errorInfo.html("Podany email jest nieprawidłowy");
+//         if (data.code === 2) {
+//           errorInfo.html(
+//               "Uruchomiłeś co najmniej cztery wersje testowe sklepu w zbyt krótkim czasie. Odczekaj 24h od ostatniej udanej próby, zanim zrobisz to ponownie."
+//             );
+//         }
+//       }
+//     }
+//   });
+// });
