0e7f7308976dc7b20e820c764a703ebb82c77960
uplostudio <110473855+uplostudio@users.noreply.github.com>
2022-10-20 13:01:38 +0200

    Update trial-step-1.js

--- trial-step-1.js
+++ trial-step-1.js
@@ -2,6 +2,9 @@ let inputsStepOne = document.querySelectorAll("[app='create_trial_step1'] input:
 
    let trialOpen = document.querySelectorAll("[app='open_trial_modal_button']");
 
+
+console.log(trialOpen)
+
   trialOpen.forEach((n) => {
     n.addEventListener("click", () => {
         document.querySelector("[app='create_trial_step1_modal']").classList.add("modal--open");
@@ -12,14 +15,16 @@ inputsStepOne.forEach((n) => {
   // Control Blur Step One
   n.addEventListener("blur", () => {
     let data;
-    let element = document.querySelector("[data-name='create_trial_step1']");
-    let elementId = element.id;
+    let element = document.querySelector("[app='create_trial_step1']");
+    console.log(element)
+    let elementId = element.getAttribute("app");
+    console.log(elementId);
 
     if (window.dataLayer) {
       data = {
         event: 'controlBlur',
         formId: elementId,
-        controlName: n.getAttribute("data-name"),
+        controlName: n.getAttribute("app"),
         controlType: n.type,
         controlValue: n.value
       };
@@ -31,14 +36,14 @@ inputsStepOne.forEach((n) => {
   // Control Focus Step One
   n.addEventListener("focus", () => {
     let data;
-    let element = document.querySelector("[data-name='create_trial_step1']");
-    let elementId = element.id;
+    let element = document.querySelector("[app='create_trial_step1']");
+    let elementId = element.getAttribute("app");
 
     if (window.dataLayer) {
       data = {
         event: 'controlFocus',
         formId: elementId,
-        controlName: n.getAttribute("data-name"),
+        controlName: n.getAttribute("app"),
         controlType: n.type,
         controlValue: n.value
       };
@@ -54,6 +59,7 @@ let createTrialStepOne = document.querySelectorAll(
   "[app='create_trial_step1']"
 );
 
+console.log(createTrialStepOne)
 
 createTrialStepOne.forEach((n) => {
   n.addEventListener("submit", (e) => {
@@ -73,9 +79,9 @@ createTrialStepOne.forEach((n) => {
     success: function (data) {
       console.log(data);
       if (data.status === 1) {
-  document
-          .querySelector("[app='create_trial_step1_modal']")
-          .classList.remove("modal--open");
+      
+  // document.querySelector("[app='create_trial_step1_modal']")
+  //         .classList.remove("modal--open");
         document
           .querySelector("[modal='create_trial_step2']")
           .classList.add("modal--open");
@@ -118,6 +124,9 @@ createTrialStepOne.forEach((n) => {
   });
   });
 });
+
+
+
 
 
 
