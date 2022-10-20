b11053e31e5139439608d45358587711e6dad2e8
uplostudio <110473855+uplostudio@users.noreply.github.com>
2022-10-20 09:51:32 +0200

    Update trial-step-2.js

--- trial-step-2.js
+++ trial-step-2.js
@@ -1,50 +1,50 @@
-// let inputsStepTwo = document.querySelectorAll("[app='create_trial_step2'] input:not([type='radio']):not([type='checkbox']):not([type='password']):not([type='submit'])")
+let inputsStepTwo = document.querySelectorAll("[app='create_trial_step2'] input:not([type='radio']):not([type='checkbox']):not([type='password']):not([type='submit'])")
 
 
-// inputsStepTwo.forEach((n) => {
-//   // Control Blur Step Two
-//   n.addEventListener("blur", () => {
-//     let data;
-//     let element = document.querySelector("[data-name='create_trial_step2']");
-//     let elementId = element.id;
+inputsStepTwo.forEach((n) => {
+  // Control Blur Step Two
+  n.addEventListener("blur", () => {
+    let data;
+    let element = document.querySelector("[data-name='create_trial_step2']");
+    let elementId = element.id;
 
-//     if (window.dataLayer) {
-//       data = {
-//         event: 'controlBlur',
-//         formId: elementId,
-//         controlName: n.getAttribute("data-name"),
-//         controlType: n.type,
-//         controlValue: n.value
-//       };
+    if (window.dataLayer) {
+      data = {
+        event: 'controlBlur',
+        formId: elementId,
+        controlName: n.getAttribute("data-name"),
+        controlType: n.type,
+        controlValue: n.value
+      };
 
-//       dataLayer.push(data);
-//       console.log(dataLayer);
-//     }
-//   })
-//   // Control Focus Step Two
-//   n.addEventListener("focus", () => {
-//     let data;
-//     let element = document.querySelector("[data-name='create_trial_step2']");
-//     let elementId = element.id;
+      dataLayer.push(data);
+      console.log(dataLayer);
+    }
+  })
+  // Control Focus Step Two
+  n.addEventListener("focus", () => {
+    let data;
+    let element = document.querySelector("[data-name='create_trial_step2']");
+    let elementId = element.id;
 
-//     if (window.dataLayer) {
-//       data = {
-//         event: 'controlFocus',
-//         formId: elementId,
-//         controlName: n.getAttribute("data-name"),
-//         controlType: n.type,
-//         controlValue: n.value
-//       };
+    if (window.dataLayer) {
+      data = {
+        event: 'controlFocus',
+        formId: elementId,
+        controlName: n.getAttribute("data-name"),
+        controlType: n.type,
+        controlValue: n.value
+      };
 
-//       dataLayer.push(data);
-//       console.log(dataLayer);
-//     }
-//   })
-// })
+      dataLayer.push(data);
+      console.log(dataLayer);
+    }
+  })
+})
 
-// let createTrialStepTwo = document.querySelectorAll(
-//   "[app='create_trial_step2']"
-// );
+let createTrialStepTwo = document.querySelectorAll(
+  "[app='create_trial_step2']"
+);
 
 
 // createTrialStepTwo.forEach((n) => {
@@ -69,6 +69,7 @@
 
 //       if (status === 200) {
 //         // MyTrackEvent Success (Step Two)
+          window.location.href = "https://www.shoper.pl/zaloz-sklep/";
 //         if (window.dataLayer) {
 //           data = {
 //             eventCategory: "Button form sent",
@@ -82,7 +83,7 @@
 //         }
 //       } else {
 //         errorInfo.css("display", "block");
-//         errorInfo.html("Podany email jest nieprawidłowy");
+            errorInfo.html("Podany numer jest nieprawidłowy");
 //         // MyTrackEvent Error (Step Two)
 //         if (window.dataLayer) {
 //           data = {
@@ -103,26 +104,26 @@
 
 
 
-$("[app='create_trial_step2']").on("submit", function (event) {
-  event.preventDefault();
-  event.stopPropagation();
-  const errorInfo = $(this).find(".w-form-fail");
-  $.ajax({
-    url: "https://www.shoper.pl/ajax.php",
-    headers: {},
-    method: "POST",
-    data: {
-      action: "create_trial_step2",
-      phone: $(this).find("[app='phone']").val(),
-    },
-    success: function (data) {
-      console.log(data);
-      if (data.status === 1) {
-        window.location.href = "https://www.shoper.pl/zaloz-sklep/";
-      } else {
-        errorInfo.css("display", "block");
-        errorInfo.html("Podany numer jest nieprawidłowy");
-      }
-    }
-  });
-});
+// $("[app='create_trial_step2']").on("submit", function (event) {
+//   event.preventDefault();
+//   event.stopPropagation();
+//   const errorInfo = $(this).find(".w-form-fail");
+//   $.ajax({
+//     url: "https://www.shoper.pl/ajax.php",
+//     headers: {},
+//     method: "POST",
+//     data: {
+//       action: "create_trial_step2",
+//       phone: $(this).find("[app='phone']").val(),
+//     },
+//     success: function (data) {
+//       console.log(data);
+//       if (data.status === 1) {
+//         window.location.href = "https://www.shoper.pl/zaloz-sklep/";
+//       } else {
+//         errorInfo.css("display", "block");
+//         errorInfo.html("Podany numer jest nieprawidłowy");
+//       }
+//     }
+//   });
+// });
