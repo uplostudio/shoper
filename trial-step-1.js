7d5a1bb897b657727814794625e77ebe68356d92
uplostudio <110473855+uplostudio@users.noreply.github.com>
2022-10-25 09:33:47 +0200

    Update trial-step-1.js

--- trial-step-1.js
+++ trial-step-1.js
@@ -117,11 +117,12 @@ let inputsStepOne = document.querySelectorAll(
         },
         success: function (data) {
           console.log(data);
-          if (data.status === 2) {
+          if (data.code === 2) {
+            let errorInfo = n.querySelector(".w-form-fail");
             errorInfo.children[0].innerHTML = "Uruchomiłeś co najmniej cztery wersje testowe sklepu w zbyt krótkim czasie. Odczekaj 24h od ostatniej udanej próby, zanim zrobisz to ponownie.";
             errorInfo.style.display = "block";
           }
-          else if (data.status === 1) {
+          else if (data.code === 1) {
             modal.classList.remove("modal--open");
             let errorInfo = n.querySelector(".w-form-fail");
             errorInfo.style.display = "none";
