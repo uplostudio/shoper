dda4ca67a2f0c63d1d402a49e89383ed849cbf2c
uplostudio <110473855+uplostudio@users.noreply.github.com>
2022-10-25 10:08:25 +0200

    Update trial-step-1.js

--- trial-step-1.js
+++ trial-step-1.js
@@ -189,14 +189,7 @@ let inputsStepOne = document.querySelectorAll(
   //             console.log(dataLayer);
               
             }
-            // let errorInfo = n.querySelector(".w-form-fail");
-            // errorInfo.children[0].innerHTML = "Podany email jest nieprawidłowy";
-            // errorInfo.style.display = "block";
-            // if (data.code === 2) {
-            //   errorInfo.children[0].innerHTML =
-            //     "Uruchomiłeś co najmniej cztery wersje testowe sklepu w zbyt krótkim czasie. Odczekaj 24h od ostatniej udanej próby, zanim zrobisz to ponownie."
-              
-            // }
+            
           }
         },
       });
@@ -204,4 +197,39 @@ let inputsStepOne = document.querySelectorAll(
   });
   
   
+
+// $(".is-trial-popup, [app='open_trial_modal_button']").on("click", function () {
+//   $("[app='create_trial_step1_modal']").addClass("modal--open");
+// });
+
+// $("[app='create_trial_step1']").on("submit", function (event) {
+//   event.preventDefault();
+//   event.stopPropagation();
+//   const errorInfo = $(this).find(".w-form-fail");
+//   $.ajax({
+//     url: "https://www.shoper.pl/ajax.php",
+//     headers: {},
+//     method: "POST",
+//     data: {
+//       action: "create_trial_step1",
+//       email: $(this).find("[app='email']").val()
+//     },
+//     success: function (data) {
+//       console.log(data);
+//       if (data.status === 1) {
+//         $("[app='create_trial_step1_modal']").removeClass("modal--open");
+//         $("[modal='create_trial_step2']").addClass("modal--open");
+//         $("[app='trial-domain']").html(data.host);
+//       } else {
+//         errorInfo.css("display", "block");
+//         errorInfo.html("Podany email jest nieprawidłowy");
+//         if (data.code === 2) {
+//           errorInfo.html(
+//               "Uruchomiłeś co najmniej cztery wersje testowe sklepu w zbyt krótkim czasie. Odczekaj 24h od ostatniej udanej próby, zanim zrobisz to ponownie."
+//             );
+//         }
+//       }
+//     }
+//   });
+// });
  
