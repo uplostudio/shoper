7ad5238dc48d0696c97a751689d0952c309d4e16
SergiuszRozycki <sergiuszrozycki@icloud.com>
2022-11-18 14:11:19 +0100

    bcm update

--- bcm.js
+++ bcm.js
@@ -1,11 +1,61 @@
 // blackfriday modal & contact
 
-let sendBcm = document.querySelectorAll("[app='bcm']");
+let sendBcmForm = document.querySelectorAll("[app='bcm']");
+let sendBcmButton = document.querySelectorAll("[app='bcm-submit']");
+let errorBorderColor = `1px solid #eb4826`;
+let initialBorderColor = `1px solid #898989`;
 
-sendBcm.forEach((n) => {
+sendBcmButton.forEach((n) => {
+  n.addEventListener("click", (e) => {
+    let form = e.target.form;
+    let inputInFormPhone = form.querySelector("[app='phone_campaign']");
+    let phoneValue = inputInFormPhone;
+    let inputInFormEmail = form.querySelector("[app='email_campaign']");
+    let emailValue = inputInFormEmail;
+    let errorBoxPhone = inputInFormPhone.parentNode.lastChild;
+    let errorBoxMail = inputInFormEmail.parentNode.lastChild;
+
+    function useRegexPhone(phoneValue) {
+      let regex = /^\d\d\d\d\d\d\d\d\d$/;
+      return regex.test(phoneValue);
+    }
+
+    function useRegexEmail(emailValue) {
+      let regex =
+        /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+      return regex.test(emailValue);
+    }
+
+    if (phoneValue.value === "") {
+      inputInFormPhone.style.border = errorBorderColor;
+      errorBoxPhone.style.display = "flex";
+    } else if (!useRegexPhone(phoneValue.value)) {
+      inputInFormPhone.style.border = errorBorderColor;
+      errorBoxPhone.style.display = "flex";
+    } else if (useRegexPhone(phoneValue.value)) {
+      inputInFormPhone.style.border = initialBorderColor;
+      errorBoxPhone.style.display = "none";
+    }
+
+    if (emailValue.value === "") {
+      inputInFormEmail.style.border = errorBorderColor;
+      errorBoxMail.style.display = "flex";
+    } else if (!useRegexEmail(emailValue.value)) {
+      inputInFormEmail.style.border = errorBorderColor;
+      errorBoxMail.style.display = "flex";
+    } else if (useRegexEmail(emailValue.value)) {
+      inputInFormEmail.style.border = initialBorderColor;
+      errorBoxMail.style.display = "none";
+    }
+  });
+});
+
+sendBcmForm.forEach((n) => {
   n.addEventListener("submit", (e) => {
     e.preventDefault();
     e.stopPropagation();
+
+    console.log("Dssdsdd");
     $.ajax({
       url: "https://www.shoper.pl/ajax.php",
       headers: {},
