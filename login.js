a157130b7456e52a2975d0d998795b042ed37ae8
SergiuszRozycki <sergiuszrozycki@icloud.com>
2022-12-05 18:45:31 +0100

    hotfix login page, always provide a valid domain

--- login.js
+++ login.js
@@ -8,22 +8,40 @@ $("[app='login']").on("submit", function (event) {
   event.preventDefault();
   event.stopPropagation();
   const errorInfo = $(this).find(".w-form-fail");
+
+  let rawValue = document.querySelector("[app='host']").value;
+  let validDomain;
+  const regex = /www.*/gm;
+  let match = rawValue.match(regex);
+
+  if (match !== null) {
+    let rawSplittedValue = match[0].split(".");
+    let rawShifted = rawSplittedValue.shift();
+    validDomain = rawSplittedValue.join(".");
+  } else {
+    validDomain = rawValue;
+  }
+
   $.ajax({
     url: "https://www.shoper.pl/ajax.php",
     headers: {},
     method: "POST",
     data: {
       action: $("[app='login']").attr("action"),
-      host: document.querySelector("[app='host']").value
+      host: validDomain,
     },
+
     success: function (data) {
+      console.log(data);
       if (data.status === 1) {
         window.location.href = data.redirect;
       } else {
         console.log(data);
         errorInfo.css("display", "block");
-        errorInfo.html("Podaj poprawny adres sklepu w formacie nazwasklepu.pl lub https://www.nazwasklepu.pl");
+        errorInfo.html(
+          "Podaj poprawny adres sklepu w formacie nazwasklepu.pl lub https://www.nazwasklepu.pl"
+        );
       }
-    }
+    },
   });
 });
