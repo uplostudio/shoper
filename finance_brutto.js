a40f59f6c426c2b06bd988a07cb859745aca2a3d
SergiuszRozycki <sergiuszrozycki@icloud.com>
2022-12-20 10:28:50 +0100

    fix

--- finance_brutto.js
+++ finance_brutto.js
@@ -125,9 +125,9 @@ $("[app='brutto_submit']").on("click", function (e) {
         phone: phoneInputValue,
         email: emailInput,
         website: urlValue,
-        bruttoTerms: bruttoTerms.val(),
-        bruttoClause: bruttoClause.val(),
-        shoperPersonalData: shoperPersonalData.val(),
+        bruttoTerms: bruttoTerms,
+        bruttoClause: bruttoClause,
+        shoperPersonalData: shoperPersonalData,
       },
 
       success: function (data) {
