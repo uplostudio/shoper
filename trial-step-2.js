e0e4aaf8f63cb8c85201065cfc7db10d398b17f6
SergiuszRozycki <sergiuszrozycki@icloud.com>
2023-01-09 12:25:22 +0100

    analytics trial 2

--- trial-step-2.js
+++ trial-step-2.js
@@ -154,7 +154,7 @@ createTrialStepTwo.forEach((n) => {
         gclid: gclidInput.value,
         fbclid: fbclidInput.value,
         blackFridayBanner: isFromBanner,
-        analyticsId: analyticsIdInputValue.value,
+        analytics_id: analyticsIdInputValue.value,
       },
       success: function (data) {
         if (data.status === 1) {
