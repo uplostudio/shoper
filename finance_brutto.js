d8033351cd79e2c3a8ae299aba4abcc20b4f74a4
SergiuszRozycki <sergiuszrozycki@icloud.com>
2023-01-09 20:36:44 +0100

    brutto update

--- finance_brutto.js
+++ finance_brutto.js
@@ -3,100 +3,24 @@ $("[app='brutto_submit']").on("click", function (e) {
   e.stopPropagation();
 
   let form = e.target.form;
-  let nipInput = form.querySelector("[app='nipNumber']");
-  let nipValue = nipInput.value;
-  let errorBoxNip = nipInput.nextElementSibling;
-  let phoneInput = form.querySelector("[app='phone']");
-  let phoneInputValue = phoneInput.value;
-  let errorBoxPhone = phoneInput.nextElementSibling;
-  let emailInput = form.querySelector("[app='email']");
-  let emailValue = emailInput.value;
-  let errorBoxEmail = emailInput.nextElementSibling;
-  let urlInput = form.querySelector("[app='url']");
-  let urlValue = urlInput.value;
-  let errorBoxUrl = urlInput.nextElementSibling;
 
   let bruttoTerms = form.querySelector("[name='brutto_terms']");
   let bruttoClause = form.querySelector("[name='brutto_info_clause']");
   let shoperPersonalData = form.querySelector("[name='shoper_personal_data']");
 
-  function useRegexPhone(phoneInputValue) {
-    let regex = /^\d\d\d\d\d\d\d\d\d$/;
-    return regex.test(phoneInputValue);
-  }
+  useRegexPhone(phoneInputValue);
+  useRegexNip(nipValue);
+  useRegexEmail(emailValue);
+  useRegexUrl(urlValue);
 
-  function useRegexNip(nipValue) {
-    let regex = /^\d\d\d\d\d\d\d\d\d$/;
-    return regex.test(nipValue);
-  }
+  checkNip(e);
+  checkPhone(e);
+  checkEmail(e);
+  checkUrl(e);
 
-  function useRegexEmail(emailValue) {
-    let regex =
-      /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    return regex.test(emailValue);
-  }
-
-  function useRegexUrl(urlValue) {
-    let regex =
-      /((([A-Za-z]{3,9}:(?:\/\/)?)(?:[-;:&=\+\$,\w]+@)?[A-Za-z0-9.-]+|(?:www.|[-;:&=\+\$,\w]+@)[A-Za-z0-9.-]+)((?:\/[\+~%\/.\w-_]*)?\??(?:[-\+=&;%@.\w_]*)#?(?:[\w]*))?)/;
-    return regex.test(urlValue);
-  }
-
-  if (nipValue === "") {
-    nipInput.style.border = errorBorderColor;
-    errorBoxNip.style.display = "flex";
-    errorBoxNip.children[1].textContent = "To pole jest wymagane";
-  } else if (!useRegexNip(nipValue)) {
-    nipInput.style.border = errorBorderColor;
-    errorBoxNip.style.display = "flex";
-    errorBoxNip.children[1].textContent = "Podaj poprawne dane";
-  } else if (useRegexNip(nipValue)) {
-    nipInput.style.border = initialBorderColor;
-    errorBoxNip.style.display = "none";
-  }
-
-  if (emailValue === "") {
-    emailInput.style.border = errorBorderColor;
-    errorBoxEmail.style.display = "flex";
-    errorBoxEmail.children[1].textContent = "To pole jest wymagane";
-  } else if (!useRegexEmail(emailValue)) {
-    emailInput.style.border = errorBorderColor;
-    errorBoxEmail.style.display = "flex";
-    errorBoxEmail.children[1].textContent = "Podaj poprawne dane";
-  } else if (useRegexEmail(emailValue)) {
-    emailInput.style.border = initialBorderColor;
-    errorBoxEmail.style.display = "none";
-  }
-
-  if (phoneInputValue === "") {
-    phoneInput.style.border = errorBorderColor;
-    errorBoxPhone.style.display = "flex";
-    errorBoxPhone.children[1].textContent = "To pole jest wymagane";
-  } else if (!useRegexPhone(phoneInputValue)) {
-    phoneInput.style.border = errorBorderColor;
-    errorBoxPhone.style.display = "flex";
-    errorBoxPhone.children[1].textContent = "Podaj poprawne dane";
-  } else if (useRegexPhone(phoneInputValue)) {
-    phoneInput.style.border = initialBorderColor;
-    errorBoxPhone.style.display = "none";
-  }
-
-  if (urlValue === "") {
-    urlInput.style.border = errorBorderColor;
-    errorBoxUrl.style.display = "flex";
-    errorBoxUrl.children[1].textContent = "To pole jest wymagane";
-  } else if (!useRegexUrl(urlValue)) {
-    urlInput.style.border = errorBorderColor;
-    errorBoxUrl.style.display = "flex";
-    errorBoxUrl.children[1].textContent = "Podaj poprawne dane";
-  } else if (useRegexUrl(urlValue)) {
-    urlInput.style.border = initialBorderColor;
-    errorBoxUrl.style.display = "none";
-  }
   if (!bruttoTerms.checked) {
     bruttoTerms.previousElementSibling.style.border = errorBorderColor;
     bruttoTerms.parentNode.nextElementSibling.style.display = "flex";
-    console.log(bruttoTerms.parentNode.nextElementSibling);
     bruttoTerms.value === 0;
   } else {
     bruttoTerms.previousElementSibling.style.border = initialBorderColor;
