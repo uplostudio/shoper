1d72ba3a50c83e99bd7fe0f02ba881123fa41203
SergiuszRozycki <sergiuszrozycki@icloud.com>
2023-01-10 20:30:13 +0100

    booste update

--- finance_booste.js
+++ finance_booste.js
@@ -3,18 +3,16 @@ $("[app='booste_submit']").on("click", function (e) {
   e.stopPropagation();
 
   let form = e.target.form;
-  let nameInput = form.querySelector("[app='firstName']");
-  let nameValue = nameInput.value;
-  let errorBoxName = nameInput.nextElementSibling;
-  let lastNameInput = form.querySelector("[app='lastName']");
-  let lastNameInputValue = lastNameInput.value;
-  let errorBoxLastName = lastNameInput.nextElementSibling;
-  let emailInput = form.querySelector("[app='email']");
-  let emailValue = emailInput.value;
-  let errorBoxEmail = emailInput.nextElementSibling;
-  let urlInput = form.querySelector("[app='url']");
-  let urlValue = urlInput.value;
-  let errorBoxUrl = urlInput.nextElementSibling;
+
+  useRegexFirstName(firstNameValue);
+  useRegexLastName(lastNameValue);
+  useRegexEmail(emailValue);
+  useRegexUrl(urlValue);
+
+  checkFirstName(e);
+  checkLastName(e);
+  checkEmail(e);
+  checkUrl(e);
 
   let shoperTermsEmail = form.querySelector("[name='shoper_terms_email']");
   let shoperTermsSms = form.querySelector("[name='shoper_terms_sms']");
@@ -23,71 +21,6 @@ $("[app='booste_submit']").on("click", function (e) {
   // let boosteTermsEmail = form.querySelector("[name='booste_terms_email']");
   // let boosteTermsSms = form.querySelector("[name='booste_terms_sms']");
   // let boosteTermsTel = form.querySelector("[name='booste_terms_tel']");
-
-  function useRegexName(nameValue) {
-    let regex =
-      /^[a-zA-ZàáâäãåąčćęèéêëėįìíîïłńòóôöõøùúûüųūÿýżźñçčšžÀÁÂÄÃÅĄĆČĖĘÈÉÊËÌÍÎÏĮŁŃÒÓÔÖÕØÙÚÛÜŲŪŸÝŻŹÑßÇŒÆČŠŽ∂ð ,.'-]+$/u;
-    return regex.test(nameValue);
-  }
-
-  function useRegexLastName(lastNameInputValue) {
-    let regex =
-      /^[a-zA-ZàáâäãåąčćęèéêëėįìíîïłńòóôöõøùúûüųūÿýżźñçčšžÀÁÂÄÃÅĄĆČĖĘÈÉÊËÌÍÎÏĮŁŃÒÓÔÖÕØÙÚÛÜŲŪŸÝŻŹÑßÇŒÆČŠŽ∂ð ,.'-]+$/u;
-    return regex.test(lastNameInputValue);
-  }
-
-  function useRegexEmail(emailValue) {
-    let regex =
-      /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    return regex.test(emailValue);
-  }
-
-  if (nameValue === "") {
-    nameInput.style.border = errorBorderColor;
-    errorBoxName.style.display = "flex";
-    errorBoxName.children[1].textContent = "To pole jest wymagane";
-  } else if (!useRegexName(nameValue)) {
-    nameInput.style.border = errorBorderColor;
-    errorBoxName.style.display = "flex";
-    errorBoxName.children[1].textContent = "Podaj poprawne dane";
-  } else if (useRegexName(nameValue)) {
-    nameInput.style.border = initialBorderColor;
-    errorBoxName.style.display = "none";
-  }
-
-  if (lastNameInputValue === "") {
-    lastNameInput.style.border = errorBorderColor;
-    errorBoxLastName.style.display = "flex";
-    errorBoxLastName.children[1].textContent = "To pole jest wymagane";
-  } else if (!useRegexLastName(lastNameInputValue)) {
-    lastNameInput.style.border = errorBorderColor;
-    errorBoxLastName.style.display = "flex";
-    errorBoxLastName.children[1].textContent = "Podaj poprawne dane";
-  } else if (useRegexLastName(lastNameInputValue)) {
-    lastNameInput.style.border = initialBorderColor;
-    errorBoxLastName.style.display = "none";
-  }
-
-  if (emailValue === "") {
-    emailInput.style.border = errorBorderColor;
-    errorBoxEmail.style.display = "flex";
-    errorBoxEmail.children[1].textContent = "To pole jest wymagane";
-  } else if (!useRegexEmail(emailValue)) {
-    emailInput.style.border = errorBorderColor;
-    errorBoxEmail.style.display = "flex";
-    errorBoxEmail.children[1].textContent = "Podaj poprawne dane";
-  } else if (useRegexEmail(emailValue)) {
-    emailInput.style.border = initialBorderColor;
-    errorBoxEmail.style.display = "none";
-  }
-
-  if (urlValue === "") {
-    urlInput.style.border = errorBorderColor;
-    errorBoxUrl.style.display = "flex";
-  } else {
-    urlInput.style.border = initialBorderColor;
-    errorBoxUrl.style.display = "none";
-  }
 
   if (!acceptAgree.checked) {
     acceptAgree.parentElement.children[0].style.border = errorBorderColor;
