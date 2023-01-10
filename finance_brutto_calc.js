b8b770fc0c7c3ae8a5b34f620cec4e6ba617a15b
SergiuszRozycki <sergiuszrozycki@icloud.com>
2023-01-10 19:52:33 +0100

    fix

--- finance_brutto_calc.js
+++ finance_brutto_calc.js
@@ -1,12 +1,13 @@
 let bruttoForm = document.querySelector("#wf-form-Brutto-Form");
-
 var slider = document.getElementById("brutto_slider");
-let amountValue = 0;
-
-console.log(amountValue);
+let amountValue = 1;
+let totalCost = document.querySelector("[app='total_cost']");
+let repaymentAmmount = document.querySelector("[app='repayment_amount']");
+let monthlyCost = document.querySelector("[app='monthly_cost']");
+let rrso = document.querySelector("[app='annual_percentage_rate_of_charge']");
 
 noUiSlider.create(slider, {
-  start: [0],
+  start: [1],
   connect: true,
   range: {
     min: 0,
@@ -25,8 +26,8 @@ bruttoForm.addEventListener("change", () => {
   let installmentPeriod = document.querySelector(
     'input[name="installment_amount"]:checked'
   ).value;
-  console.log(installmentPeriod);
-  console.log(amountValue);
+  // console.log(installmentPeriod);
+  // console.log(amountValue);
 
   fetch(
     `https://www.brutto.pl/api/v3/simulation/purchase?partner=e8694f3a-45fe-5fa6-bb74-ccc1e8f16d32&currency=PLN&amount=${amountValue}`,
@@ -36,7 +37,23 @@ bruttoForm.addEventListener("change", () => {
       },
       method: "GET",
     }
-  ).then(function (response) {
-    console.log(response);
-  });
+  )
+    .then((response) => {
+      return response.json();
+    })
+    .then((data) => {
+      let obj = data.purchase_simulation.results;
+      let picked = Object.fromEntries(
+        Object.entries(obj).filter(([key]) =>
+          key.includes(`${installmentPeriod}`)
+        )
+      );
+      // console.log(installmentPeriod)
+      // console.log(data.purchase_simulation.results)
+      console.log(picked);
+      // totalCost.innerHTML = picked.total_cost;
+      // repaymentAmmount.innerHTML = picked.repayment_amount;
+      // monthlyCost.innerHTML = picked.monthly_cost;
+      // rrso.innerHTML = picked.annual_percentage_rate_of_charge;
+    });
 });
