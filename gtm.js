78d135539b6b11eaf8dfb35779a90c283054d146
SergiuszRozycki <sergiuszrozycki@icloud.com>
2023-01-12 10:35:49 +0100

    google bar

--- gtm.js
+++ gtm.js
@@ -13,3 +13,19 @@ window.addEventListener("load", () => {
     });
   } catch (err) {}
 });
+
+setInterval(function () {
+  try {
+    let menu = document.querySelector(".nav__menu");
+    let banner = document.querySelector("#top-bar-header");
+    bannerHeightString = window.getComputedStyle(banner).height;
+    bannerHeightValue = parseInt(bannerHeightString);
+
+    if (window.innerWidth <= 991 && window.scrollY < 30) {
+      menu.style.height = `${window.innerHeight - bannerHeightValue}px`;
+      console.log(menu.style.height);
+    } else {
+      menu.style.height = `${window.innerHeight}px`;
+    }
+  } catch (err) {}
+}, 100);
