733003d94934defb687b503ffb4b25beff6e7fd8
SergiuszRozycki <sergiuszrozycki@icloud.com>
2023-02-02 12:51:45 +0100

    revert

--- gtm.js
+++ gtm.js
@@ -21,47 +21,7 @@ window.addEventListener("DOMContentLoaded", () => {
   // if (subpage !== "/rodo") {
   let intercomSrc = "https://shoper-web.netlify.app/intercom.js";
   let intercomScript = document.createElement("script");
-  // intercomScript.src = intercomSrc;
-  intercomScript.innerHTML = `window.intercomSettings = {
-    api_base: "https://api-iam.intercom.io",
-    app_id: "w3fwiuib",
-  };
-  
-  // We pre-filled your app ID in the widget URL: 'https://widget.intercom.io/widget/w3fwiuib'
-  (function () {
-    var w = window;
-    var ic = w.Intercom;
-    if (typeof ic === "function") {
-      ic("reattach_activator");
-      ic("update", w.intercomSettings);
-    } else {
-      var d = document;
-      var i = function () {
-        i.c(arguments);
-      };
-      i.q = [];
-      i.c = function (args) {
-        i.q.push(args);
-      };
-      w.Intercom = i;
-      var l = function () {
-        var s = d.createElement("script");
-        s.type = "text/javasr.ccript";
-        s.async = true;
-        s.src = "https://widget.intercom.io/widget/w3fwiuib";
-        var x = d.getElementsByTagName("script")[0];
-        x.parentNode.insertBefore(s, x);
-      };
-      if (document.readyState === "complete") {
-        l();
-      } else if (w.attachEvent) {
-        w.attachEvent("onload", l);
-      } else {
-        w.addEventListener("load", l, false);
-      }
-    }
-  })();
-  `;
+  intercomScript.src = intercomSrc;
   body.append(intercomScript);
   // } else {
   // }
