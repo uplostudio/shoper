34b37e7b8c3d8f919d1b424ddf0f6e5b8f8a7b63
SergiuszRozycki <sergiuszrozycki@icloud.com>
2023-02-02 14:01:17 +0100

    added intercom

--- gtm.js
+++ gtm.js
@@ -13,16 +13,16 @@ window.addEventListener("load", () => {
     });
   } catch (err) {}
 });
-// window.addEventListener("DOMContentLoaded", () => {
-// let subpage = window.location.pathname;
-// let body = document.querySelector("body");
-// let scripts = head.querySelectorAll("script");
 
-// if (subpage !== "/rodo") {
-// let intercomSrc = "https://shoper-web.netlify.app/intercom.js";
-// let intercomScript = document.createElement("script");
-// intercomScript.src = intercomSrc;
-// body.append(intercomScript);
-// } else {
-// }
-// });
+window.addEventListener("load", () => {
+  let subpage = window.location.pathname;
+  let body = document.querySelector("body");
+
+  if (subpage !== "/rodo") {
+    let intercomSrc = "https://shoper-web.netlify.app/intercom.js";
+    let intercomScript = document.createElement("script");
+    intercomScript.src = intercomSrc;
+    body.append(intercomScript);
+  } else {
+  }
+});
