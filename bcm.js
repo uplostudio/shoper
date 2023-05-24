e1fd470e41c7ce50877af86035816c2f65f55be2
Sergiusz Różycki <sergiuszrozycki@MacBook-Air-Sergiusz.local>
2023-05-24 15:12:56 +0200

    added enter action to bcm

--- bcm.js
+++ bcm.js
@@ -16,6 +16,9 @@ formTrigger = formWrapper.querySelector("[app='bcm-submit']");
 phoneInput = formWrapper.querySelector("[app='phone_campaign']");
 emailInput = formWrapper.querySelector("[app='email_campaign']");
 
+phoneInput.addEventListener("keydown", createEnterKeydownHandler(phoneInput, formTrigger));
+emailInput.addEventListener("keydown", createEnterKeydownHandler(emailInput, formTrigger));
+
 // Attach EventListeners to inputs
 
 emailInput.addEventListener("blur", function () {
