998f92cf3e60bbb36f1d2c0817c1e7496150aac7
SergiuszR <sergiuszrozycki@icloud.com>
2023-09-14 12:28:03 +0200

    dynamical host name

--- trial-step-one.js
+++ trial-step-one.js
@@ -119,6 +119,7 @@ $(document).ready(function () {
 
           if (data.status === 1) {
             $("[data-app='create_trial_step1_modal']").removeClass("modal--open");
+            $("[data-app='trial-domain']").text(window.myGlobals.host);
             $('[data-modal="create_trial_step2"]').addClass("modal--open");
 
             DataLayerGatherers.pushEmailSubmittedData(window.myGlobals.clientId, window.myGlobals.shopId, $("#create_trial_step1").attr("data-action"), emailField.val());
