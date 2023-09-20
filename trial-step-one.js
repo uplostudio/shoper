f5e28d33ffff6fcefcf4ad389111ba6d5dab75e6
SergiuszR <sergiuszrozycki@icloud.com>
2023-09-20 16:10:49 +0200

    test 1

--- trial-step-one.js
+++ trial-step-one.js
@@ -3,7 +3,6 @@ $(document).ready(function () {
     $("[data-app='create_trial_step1_modal']").addClass("modal--open");
     $("body").addClass("overflow-hidden");
   });
-
   let state = {
     errors: [],
     analyticsId: "",
@@ -13,12 +12,10 @@ $(document).ready(function () {
       '^(([^<>()\\[\\]\\\\.,;:\\s@"]+(\\.[^<>()\\[\\]\\\\.,;:\\s@"]+)*)|(\\".+\\"))@((\\[[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}\\])|(([a-zA-Z\\-0-9]+\\.)+[a-zA-Z]{2,}))$'
     ),
   };
-
   function getOrStoreParameter(name) {
     let urlSearchParams = new URLSearchParams(window.location.search);
     let urlValue = urlSearchParams.get(name) || "";
     let storedValue = localStorage.getItem(name);
-
     if (urlValue) {
       if (urlValue !== storedValue) {
         localStorage.setItem(name, urlValue);
@@ -29,7 +26,6 @@ $(document).ready(function () {
     }
     return "";
   }
-
   function updateAnalytics() {
     setTimeout(function () {
       try {
@@ -40,48 +36,40 @@ $(document).ready(function () {
       } catch (err) {}
     }, 2000);
   }
-
   function setupValidation() {
     const forms = $('[data-action="create_trial_step1"]');
     forms.each(function () {
       const form = $(this);
       const emailField = form.find('[data-type="email"]');
-
       // Add "touched" data to false on creation
       emailField.data("touched", false);
-
       emailField.on("blur", function () {
         // Only if the field was touched run validation
         if ($(this).data("touched")) {
           state.errors = validateEmail(this, state.errors, state.emailRegex);
         }
       });
-
       emailField.on("keydown", function (e) {
         // On first keydown (excluding Tab), set touched data to true
         if (!$(this).data("touched") && e.which !== 9) {
           $(this).data("touched", true);
         }
-
         if (e.which === 13) {
           emailField.trigger("blur");
           onSubmitClick(e, emailField, form);
         }
       });
-
-      // emailField
-      //   .closest("form")
-      //   .find('[data-form="submit-step-one"]')
-      //   .on("click", function (e) {
-      //     onSubmitClick(e, emailField, form);
-      //   });
+      emailField
+        .closest("form")
+        .find('[data-form="submit-step-one"]')
+        .on("click", function (e) {
+          onSubmitClick(e, emailField, form);
+        });
     });
   }
-
   function validateEmail(field, errors, emailRegex) {
     let email = $(field).val();
     $(field).removeClass("error");
-
     if (!email) {
       $(field).addClass("error").next(".for-empty").show();
       errors.push("Email is required.");
@@ -91,19 +79,15 @@ $(document).ready(function () {
     }
     return errors;
   }
-
   function onSubmitClick(e, emailField, form) {
-    e.preventDefault();
     state.errors = [];
     const wFormFail = form.next().next();
     $(emailField).trigger("blur");
-
     const statusMessages = {
       2: "Próbujesz uruchomić więcej niż jedną wersję testową sklepu w zbyt krótkim czasie. Odczekaj co najmniej godzinę, zanim zrobisz to ponownie.",
       3: "Próbujesz uruchomić więcej niż jedną wersję testową sklepu w zbyt krótkim czasie. Odczekaj kilka minut, zanim zrobisz to ponownie.",
       4: "Uruchomiłeś co najmniej cztery wersje testowe sklepu w zbyt krótkim czasie. Odczekaj 24h od ostatniej udanej próby, zanim zrobisz to ponownie.",
     };
-
     if (state.errors.length === 0) {
       const loader = form.find(".loading-in-button");
       $.ajax({
@@ -123,25 +107,19 @@ $(document).ready(function () {
           if (data.client_id) window.myGlobals.clientId = data.client_id;
           if (data.host) window.myGlobals.host = data.host;
           if (data.shop_id) window.myGlobals.shopId = data.shop_id;
-
           if (data.code > 0) {
             $(wFormFail).text(statusMessages[data.code]);
             $(wFormFail).show();
           }
-
           if (data.status === 1) {
             $("[data-app='create_trial_step1_modal']").removeClass("modal--open");
             $("[data-app='trial-domain']").text(window.myGlobals.host);
             $('[data-modal="create_trial_step2"]').addClass("modal--open");
-
             DataLayerGatherers.pushEmailSubmittedData(window.myGlobals.clientId, window.myGlobals.shopId, $("#create_trial_step1").attr("data-action"), emailField.val());
-
             DataLayerGatherers.pushFormSubmitSuccessData($("#create_trial_step1").attr("data-action"), emailField.val());
-
             DataLayerGatherers.pushTrackEventData($("#create_trial_step1").attr("data-action"), $("#create_trial_step1").find("#label").text(), emailField.val());
           } else {
             DataLayerGatherers.pushTrackEventError($("#create_trial_step1").attr("data-action"), $("#create_trial_step1").find("#label").text(), emailField.val());
-
             DataLayerGatherers.pushSubmitError($("#create_trial_step1").attr("data-action"), $("#create_trial_step1").find("#label").text(), emailField.val());
           }
         },
@@ -158,7 +136,6 @@ $(document).ready(function () {
       console.log(state.errors);
     }
   }
-
   $('[data-form="submit-step-one"]').each(function () {
     const form = $(this).closest("form");
     const emailField = form.find('[data-type="email"]');
@@ -166,7 +143,6 @@ $(document).ready(function () {
       onSubmitClick(e, emailField, form);
     });
   });
-
   updateAnalytics();
   setupValidation();
 });
