7df3f37387ba7899e770c24b452fa940c0b37cf5
SergiuszR <sergiuszrozycki@icloud.com>
2023-10-16 18:24:33 +0200

    inital commit

--- hr/main.js
+++ hr/main.js
@@ -80,38 +80,34 @@ function adjustBoxPosition() {
 }
 
 // Trigger on window resize and on load
-$(window).on("load resize", adjustBoxPosition);
-
-// ScrollTrigger imported
-
-gsap.registerPlugin(ScrollTrigger);
-
 $(document).ready(function () {
   var boxEl = $('[data-box="job-cta"]');
+  var slideUpAnimation = { duration: 0.3, bottom: "0", ease: "power1.out" };
+  var slideDownAnimation = { duration: 0.3, bottom: "-100%", ease: "power1.out" };
 
   if ($(window).width() < 992) {
     boxEl.css({
       position: "fixed",
-      bottom: "-100%",
+      top: "auto",
+      bottom: "-100%", // initially hidden
     });
 
-    var boxAnimation = gsap.timeline({
-      scrollTrigger: {
-        trigger: "#offer-hero",
-        start: "top 80%",
-        end: "bottom 10%",
-        toggleActions: "play none none reverse",
-      },
+    ScrollTrigger.create({
+      trigger: "#offer-hero",
+      start: "bottom 90%", // starts when #offer-hero enters from the top
+      end: "bottom 20%", // ends when #offer-hero leaves from top
+      onLeave: () => gsap.to(boxEl, slideUpAnimation),
+      onEnterBack: () => gsap.to(boxEl, slideDownAnimation),
+      scrub: true,
     });
-
-    boxAnimation.to(boxEl, { duration: 0.3, bottom: 0 });
 
     ScrollTrigger.create({
       trigger: "#offer-footer",
-      start: "top 90%",
-      onEnter: () => {
-        gsap.to(boxEl, { duration: 0.3, bottom: "-100%" });
-      },
+      start: "top bottom", // starts when #offer-footer enters from the bottom
+      end: "top top", // ends when #offer-footer reaches the top
+      onEnter: () => gsap.to(boxEl, slideDownAnimation),
+      onLeaveBack: () => gsap.to(boxEl, slideUpAnimation),
+      scrub: true,
     });
   }
 });
