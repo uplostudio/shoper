c4edb3717f41b07dabc559d07bf135cd342751c7
Sergiusz Rozycki <sergiuszrozycki@icloud.com>
2023-11-06 10:56:28 +0100

    Update trial-step-one.js

--- trial-step-one.js
+++ trial-step-one.js
@@ -107,7 +107,7 @@ $(document).ready(function () {
         type: "POST",
         url: window.myGlobals.URL,
         data: {
-          action: $("form").attr("data-action"),
+          action: $(form).attr("data-action"),
           email: $(emailField).val(),
           "adwords[gclid]": state.gclidValue,
           "adwords[fbclid]": state.fbclidValue,
