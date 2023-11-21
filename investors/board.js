efb8c693141a2c73dde67a325f90523069bb1239
Sergiusz Rozycki <sergiuszrozycki@icloud.com>
2023-11-21 12:01:49 +0100

    Create board.js

---
+++ investors/board.js
@@ -0,0 +1,5 @@
+/*
+ * File used for controlling the display of board members' modals on the page.
+ * The operation principle is based on handling clicks on individual elements, 
+ * which are fetched using appropriate attributes.
+ */
