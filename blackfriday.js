d64af062fb377482f7f7c6f923fb1b2f006d38f0
Sergiusz Różycki <sergiusz@iMac-Sergiusz.lan>
2024-01-08 10:57:09 +0100

    change disclaimer in login modal

--- blackfriday.js
+++ blackfriday.js
@@ -1,9 +1,12 @@
+// modal disclaimer update based on url
+
 $(document).ready(function () {
   updateDisclaimer();
 });
 
 const pathnameGroups = {
   group1: ["/kampanie-ppc/", "/shoper-kampanie/", "/google-shopping/", "/microsoft-ads/", "/facebook/", "/instagram/", "/reklama-tiktok/"],
+  group2: ["/systemy-platnosci/", "/kurierzy/", "/domena/", "/certyfikaty-ssl/", "/klarna/", "/systemy-platnosci/google-pay/", "/systemy-platnosci/paypo/"],
 };
 
 function getGroupForPathname(pathname) {
@@ -22,9 +25,9 @@ function updateDisclaimer() {
   let currentPathname = window.location.pathname;
 
   const currentGroup = getGroupForPathname(currentPathname);
+  const dataItem = $("[data-item='disclaimer']");
 
   if (currentGroup === "group1") {
-    const dataItem = $("[data-item='disclaimer']");
     const disclaimerLink1 = `<a href="https://www.shoper.pl/static/rodo/Shoper_klauzula_informacyjna_osoby_korespondujace_z_Shoper.pdf" target="_blank" class="inline-link">informacją na temat przetwarzania twoich danych osobowych</a>`;
     const disclaimerLink2 = `<a href="https://www.shoper.pl/static/regulaminy/ogloszenie-o-promocji-shoper-kampanie-oferta-noworoczna-2023-2024.pdf" target="_blank" class="inline-link"> ogłoszenie o promocji Oferta Noworoczna 2023/2024.</a>`;
     const disclaimerText = `Wysyłając wiadomość potwierdzasz zapoznanie się z ${disclaimerLink1} oraz ${disclaimerLink2}`;
@@ -34,5 +37,9 @@ function updateDisclaimer() {
     const prependHTML = `<div class="badge-new is-new-pricing"><div><strong>Teraz darmowa konsultacja + 50% rabatu na obsługę</strong></div></div>`;
     const parentForm = dataItem.closest("form");
     parentForm.prepend(prependHTML);
+  } else if (currentGroup === "group2") {
+    const disclaimerHTML = `<div>Nie masz jeszcze sklepu? <a href="https://www.shoper.pl/cennik-sklepu-shoper/" class="inline-link">Wypróbuj go za darmo</a></div>`;
+
+    dataItem.html(disclaimerHTML);
   }
 }
