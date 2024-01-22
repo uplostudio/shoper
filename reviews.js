19537c356e18c341a6f1b5ff581f2184256d2569
Sergiusz Różycki <sergiusz@iMac-Sergiusz.local>
2024-01-22 17:37:41 +0100

    reviews update

--- reviews.js
+++ reviews.js
@@ -1,37 +1,70 @@
 $(document).ready(function () {
+  $('[data-item^="list"]').each(function () {
+    var listContainer = $(this);
+    var listToSort = [];
+
+    listContainer.find("span[fs-cmsfilter-field]").each(function () {
+      var $this = $(this);
+      var value = $this.data("value");
+      var count = $(`div[fs-cmsfilter-element='list'] div[data-set='${value}']`).length;
+
+      $this.next(".counter_span").text("[" + count + "]");
+      console.log($this[0].length);
+
+      if ($this.parent().parent().parent()) {
+        listToSort.push({
+          element: $this.parent().parent().parent(),
+          count: count,
+        });
+      }
+    });
+
+    if (listToSort.length > 0) {
+      listToSort.sort((a, b) => b.count - a.count);
+      $.each(listToSort, (index, item) => listContainer.append(item.element));
+    }
+  });
+
   $("[data-item^=list]").each(function () {
     var $this = $(this);
     var itemCount = $this.children().length;
     var itemNum = $this.attr("data-item").split("-")[1];
+
     if (itemCount > 5) {
       $this.addClass("collapsed");
       var hiddenChildren = itemCount - 5;
-      $("[data-item=expand-" + itemNum + "]")
-        .text("Pokaż wszystkie branże [" + hiddenChildren + "]")
-        .show();
+
+      if ($("[data-item=expand-" + itemNum + "]")) {
+        $("[data-item=expand-" + itemNum + "]")
+          .text("Pokaż wszystkie branże [" + hiddenChildren + "]")
+          .show();
+      }
     }
   });
 
   $("[data-item^=expand]").click(function () {
     var $this = $(this);
-    var expandNum = $(this).attr("data-item").split("-")[1];
-    $("[data-item=list-" + expandNum + "]").removeClass("collapsed");
-    $this.hide();
-  });
-});
+    var expandNum = $this.attr("data-item") ? $this.attr("data-item").split("-")[1] : "";
 
-$(document).ready(function () {
+    if (expandNum) {
+      $("[data-item=list-" + expandNum + "]").removeClass("collapsed");
+      $this.hide();
+    }
+  });
+
   $(".feed_box-bottom").each(function () {
     var $this = $(this);
+
     if ($this.find("a.w-condition-invisible").length == 1) {
       $this.find("a:not(.w-condition-invisible)").each(function () {
-        var aElement = $(this)[0];
-        var classes = aElement.classList;
-        for (var i = 0; i < classes.length; i++) {
-          if (classes[i] !== "button") {
-            $(aElement).removeClass(classes[i]);
+        var aElement = $(this);
+        var classes = aElement.prop("classList") || [];
+
+        classes.forEach(function (classItem) {
+          if (classItem !== "button") {
+            aElement.removeClass(classItem);
           }
-        }
+        });
       });
     }
   });
