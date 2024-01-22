4ddcb134e69b4342e3a78c4e02d6673d6611dad8
Michał Madera <michalmadera@MacBook-Pro-Micha.local>
2024-01-22 17:34:07 +0100

    WD-113

--- shoplo/modules/phoneNumberWithPrefix/index.js
+++ shoplo/modules/phoneNumberWithPrefix/index.js
@@ -1,22 +1,25 @@
-import intlTelInput from 'intl-tel-input';
-import 'intl-tel-input/build/css/intlTelInput.css';
+import intlTelInput from "intl-tel-input";
+import "intl-tel-input/build/css/intlTelInput.css";
+import "./style.css";
 
 const phoneNumberWithPrefixFieds = $('[data-phone-field-prefix="true"]');
 const condition = () => phoneNumberWithPrefixFieds.length > 0;
 
 const initialize = () => {
-    $(phoneNumberWithPrefixFieds).each( ( index, phoneField ) => {
-        intlTelInput(phoneField, {
-            utilsScript: "intl-tel-input/build/js/utils.js",
-        preferredCountries: ["pl", "de", "ie", "us", "gb", "nl"],
-        autoInsertDialCode: false,
-        nationalMode: false,
-        separateDialCode: true,
-        autoPlaceholder: "off",
-        initialCountry: "pl",
-        showSelectedDialCode: true,
-        });
-    } ); 
+  $(phoneNumberWithPrefixFieds).each((index, phoneField) => {
+    const iti = intlTelInput(phoneField, {
+      utilsScript: "https://cdn.jsdelivr.net/npm/intl-tel-input@19.2.12/build/js/utils.js",
+      preferredCountries: ["pl", "de", "ie", "us", "gb", "nl"],
+      autoInsertDialCode: false,
+      nationalMode: false,
+      separateDialCode: true,
+      autoPlaceholder: "off",
+      initialCountry: "pl",
+      showSelectedDialCode: true,
+      countrySearch: false
+    });
+  });
+
 };
 
 export default {
