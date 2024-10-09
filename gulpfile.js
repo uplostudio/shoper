90066499ab064efed6926a2d27ce86b4ca1dfccc
Sergiusz Różycki <sergiuszrozycki@macbook-pro-sergiusz-2.home>
2024-10-09 10:08:25 +0200

    added minified

--- gulpfile.js
+++ gulpfile.js
@@ -3,10 +3,12 @@ const uglify = require('gulp-uglify');
 const rename = require('gulp-rename');
 const concat = require('gulp-concat');
 const path = require('path');
+const fs = require('fs');
 
 const paths = {
     scripts: {
-        src: [
+        src: 'src/**/*.js',  // This will match all .js files in src and its subdirectories
+        bundleSrc: [
             'src/new_form_handler-main.js',
             'src/accordion.js',
             'src/new_trial/shared_utils.js',
@@ -19,23 +21,59 @@ const paths = {
             'src/afiliant.js',
             'src/valueTrack.js'
         ],
-        dest: 'bundle/'
+        dest: 'bundle/',
+        minDest: 'min/'
     }
 };
 
 function bundle() {
-    return gulp.src(paths.scripts.src)
+    return gulp.src(paths.scripts.bundleSrc)
         .pipe(concat('bundle.js'))
         .pipe(uglify())
         .pipe(rename({ extname: '.min.js' }))
         .pipe(gulp.dest(paths.scripts.dest));
 }
 
-function watchFiles() {
-    gulp.watch(paths.scripts.src, bundle);
+function cleanMin(cb) {
+    if (fs.existsSync(paths.scripts.minDest)) {
+        fs.rm(paths.scripts.minDest, { recursive: true, force: true }, (err) => {
+            if (err) {
+                console.error(err);
+            }
+            cb();
+        });
+    } else {
+        cb();
+    }
 }
 
-const build = gulp.series(bundle, watchFiles);
+function createMinDir(cb) {
+    if (!fs.existsSync(paths.scripts.minDest)) {
+        fs.mkdir(paths.scripts.minDest, { recursive: true }, (err) => {
+            if (err) {
+                console.error(err);
+            }
+            cb();
+        });
+    } else {
+        cb();
+    }
+}
+
+function minifyAllFiles() {
+    return gulp.src(paths.scripts.src)
+        .pipe(uglify())
+        .pipe(rename(function(path) {
+            path.extname = '.min.js';
+        }))
+        .pipe(gulp.dest(paths.scripts.minDest));
+}
+
+function watchFiles() {
+    gulp.watch(paths.scripts.src, gulp.series(bundle, cleanMin, createMinDir, minifyAllFiles));
+}
+
+const build = gulp.series(bundle, cleanMin, createMinDir, minifyAllFiles, watchFiles);
 
 exports.bundle = bundle;
 exports.watch = watchFiles;
