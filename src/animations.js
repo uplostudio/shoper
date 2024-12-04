82035fb31df585be82fb219db3a847d3ac7d7aac
Sergiusz Różycki <sergiuszrozycki@macbook-pro-sergiusz-3.home>
2024-12-04 10:29:56 +0100

    remove let vh

--- src/animations.js
+++ src/animations.js
@@ -75,7 +75,7 @@ document.documentElement.style.setProperty("--vh", `${vh}px`);
 // We listen to the resize event
 window.addEventListener("resize", () => {
   // We execute the same script as before
-  let vh = window.innerHeight * 0.01;
+  vh = window.innerHeight * 0.01;
   document.documentElement.style.setProperty("--vh", `${vh}px`);
 });
 
